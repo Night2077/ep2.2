9d37b2d9cba37df804d9d4e75e360860cca95c35
Night2077 <matheus.a.brito12@gmail.com>
2025-09-22 18:11:30 -0300

    c2

--- server.py
+++ server.py
@@ -1,5 +1,19 @@
+
+from pdb import main
 from imgen import im_generation
+#from PIL import Image
 
-img = im_generation.generate_image_macro('img.jpg', 'top text', 'bottom text')
+NOME_DA_IMAGEM = 'img.jpg'
+TEXTO_CIMA = 'Persona 6'
+TEXTO_BAIXO = 'Nunca Sai'
+
+img = im_generation.generate_image_macro(NOME_DA_IMAGEM, TEXTO_CIMA, TEXTO_BAIXO)
+
 img.show()
+img.save('output.jpg')
+print('Imagem macro gerada e salva como output.jpg')
 
+
+
+if __name__ == "__main__":
+    main()
